e5394eacffd420164fc59fb337d432140ad207e9
Dom Williams <me@domwillia.ms>
2017-04-13 17:19:48 +0200

    Change route primary key to title of 3+ chars

--- server/smart_running/models.py
+++ server/smart_running/models.py
@@ -1,5 +1,6 @@
 import djchoices
 from django.contrib.auth.models import User
+from django.core import validators
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 from django.db.models.signals import post_save
@@ -23,7 +24,7 @@ class Route(models.Model):
     date_published = models.DateTimeField(auto_now_add=True)
     last_updated = models.DateTimeField(auto_now=True)
 
-    title = models.CharField(max_length=64)
+    title = models.CharField(max_length=64, primary_key=True, validators=[validators.MinLengthValidator(3), ])
     description = models.CharField(max_length=200)
 
     mode = models.CharField(max_length=16, choices=ModeType.choices)
