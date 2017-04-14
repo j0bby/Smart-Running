de90a0215e5ddd26bdf2a09b3f59d83a7bbd141d
fadwa.messaoudi <fadwa.messaoudi@gmail.com>
2017-04-14 10:30:02 +0200

    Add app frontend

---
+++ app/src/main/java/com/example/user/jobby/SigninSignup.java
@@ -0,0 +1,38 @@
+package com.example.user.jobby;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EdgeEffect;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class SigninSignup extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin_signup);
+
+        TextView t = (TextView) findViewById(R.id.link_signup);
+        EditText textemail = (EditText) findViewById(R.id.input_email);
+        EditText textpassword = (EditText) findViewById(R.id.input_password);
+        Button buttonlogin = (Button) findViewById(R.id.button);
+
+        t.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // Start the Signup activity
+                Intent intent = new Intent(getApplicationContext(), Signup.class);
+                startActivity(intent);
+                finish();
+                overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
+            }
+        });
+    }
+}
