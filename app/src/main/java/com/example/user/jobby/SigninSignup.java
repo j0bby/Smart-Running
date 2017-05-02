4756cb3562231950ac1b149c5baa80df7327e9bf
xingyiditongxue <yidi.xing@insa-lyon.fr>
2017-05-02 14:34:05 +0800

    Modification sur le front-end pour qu'il fonctionne correctement

--- app/src/main/java/com/example/user/jobby/SigninSignup.java
+++ app/src/main/java/com/example/user/jobby/SigninSignup.java
@@ -1,6 +1,9 @@
 package com.example.user.jobby;
 
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.BitmapFactory;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,22 +12,33 @@ import android.widget.Button;
 import android.widget.EdgeEffect;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.ImageView;
+import android.graphics.Bitmap;
+
+import java.io.InputStream;
 
 public class SigninSignup extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signin_signup);
+//        setContentView(R.layout.activity_signin_signup);
+        try {
+            setContentView(R.layout.activity_signin_signup);
+            ImageView myLogoImgView = (ImageView) findViewById(R.id.imglogo);
+            myLogoImgView.setImageBitmap(decodeSampleBitmapFromResource(getResources(), R.drawable.logo, 100, 100));
+        } catch (OutOfMemoryError error) {
+            error.printStackTrace();
+        }
 
-        TextView t = (TextView) findViewById(R.id.link_signup);
+//        TextView t = (TextView) findViewById(R.id.link_signup);
+        TextView signuparea = (TextView) findViewById(R.id.link_signup);
         EditText textemail = (EditText) findViewById(R.id.input_email);
         EditText textpassword = (EditText) findViewById(R.id.input_password);
         Button buttonlogin = (Button) findViewById(R.id.button);
 
-        t.setOnClickListener(new View.OnClickListener() {
-
+//        t.setOnClickListener(new View.OnClickListener() {
+        signuparea.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Start the Signup activity
@@ -34,5 +48,37 @@ public class SigninSignup extends AppCompatActivity {
                 overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
             }
         });
+    }
+
+    public static int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        //original height and width of resource image
+        final int imgHeight = options.outHeight;
+        final int imgWidth = options.outWidth;
+        int inSampleSize = 1;
+
+        if (imgHeight > reqHeight || imgWidth > reqWidth) {
+            final int halfHeight = imgHeight / 2;
+            final int halfWidth = imgWidth / 2;
+
+            while ((halfHeight / inSampleSize) > reqHeight
+                    && (halfWidth / inSampleSize) > reqWidth) {
+                inSampleSize *= 2;
+            }
+        }
+        return inSampleSize;
+    }
+
+    public static Bitmap decodeSampleBitmapFromResource(Resources res, int resId, int reqWidth, int reqHeight) {
+        //configure inJustDecodeBoudns=true to acquire the size of resource image
+        final BitmapFactory.Options opt = new BitmapFactory.Options();
+        opt.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(res, resId, opt);
+
+        //calculate inSampleSize
+        opt.inSampleSize = calculateInSampleSize(opt, reqWidth, reqHeight);
+
+        // generate Bitmap (decoding image with inSampleSize obtained)
+        opt.inJustDecodeBounds = false;
+        return BitmapFactory.decodeResource(res, resId, opt);
     }
 }
